7a440587c1620fcb3e769f254b113b5eaabcafc2
Ben Yoon <BYoon12@gmail.com>
2023-02-25 13:50:39 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import './App.css';
-
-
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <div className="App">
       <h1>Bootstrap Website</h1>
-     
+      <Nav fill variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/about">About Us</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/contact">Contact</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <br />
+
+      <Container>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type="email" placeholder="Enter Email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Form Dropdown with favorite movies</Form.Label>
+          <Form.Control as="select">
+            <option>Inception</option>
+            <option>Forrest Gump</option>
+            <option>Shawshank Redemption</option>
+            <option>Spiderman: Into the Spiderverse</option>
+            <option>Avengers: Endgame</option>
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicCheckBox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      </Container>
+      <Button variant="danger">Click Me!</Button>
     </div>
   );
 }
